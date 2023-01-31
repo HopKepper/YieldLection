a159e5f6e1d0f75df06fd5ce91712eef8c060e05
HopKepper <viollo190201@gmail.com>
2023-01-31 10:59:47 +0200

    first commit

---
+++ YeildLection/PerfomanceExample.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeildLection
+{
+    public class PerfomanceExampleSimple
+    {
+        public int[] Array(int start, int count)
+        {
+            var numbers = new int[count];
+            for (var i = 0; i < count; ++i)
+                numbers[i] = start + i;
+
+            return numbers;
+        }
+
+        public int[] Iterator(int start, int count)
+        {
+            return IteratorInternal(start, count).ToArray();
+        }
+
+        private IEnumerable<int> IteratorInternal(int start, int count)
+        {
+            for (var i = 0; i < count; ++i)
+                yield return start + i;
+        }
+    }
+
+    public class PerfomanceExampleHard
+    {
+        public List<Tuple<int, string>> List(int start, int count)
+        {
+            var odds = new List<Tuple<int, string>>(); //First time
+            foreach (var record in OddsArray(ReadFromDb(start, count)))
+                if (record.Item1 % 3 == 0)
+                    odds.Add(record);
+
+            return odds;
+        }
+
+        public List<Tuple<int, string>> Iterator(int start, int count)
+        {
+            return IteratorInternal(start, count).ToList();
+        }
+
+        private IEnumerable<Tuple<int, string>> IteratorInternal(int start, int count)
+        {
+            foreach (var record in OddsIterator(ReadFromDb(start, count)))
+                if (record.Item1 % 3 == 0)
+                    yield return record;
+        }
+
+        private IEnumerable<Tuple<int, string>> OddsIterator(IEnumerable<Tuple<int, string>> records)
+        {
+            foreach (var record in records)
+                if (record.Item1 % 2 != 0)
+                    yield return record;
+        }
+
+        private List<Tuple<int, string>> OddsArray(IEnumerable<Tuple<int, string>> records)
+        {
+            var odds = new List<Tuple<int, string>>(); //Second time
+            foreach (var record in records)
+                if (record.Item1 % 2 != 0)
+                    odds.Add(record);
+
+            return odds;
+        }
+
+        private IEnumerable<Tuple<int, string>> ReadFromDb(int start, int count)
+        {
+            for (var i = start; i < count; ++i)
+                yield return new Tuple<int, string>(start + i, RandomString());
+        }
+
+        private static string RandomString()
+        {
+            return Guid.NewGuid().ToString("n");
+        }
+    }
+}
---
+++ YeildLection/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+
+namespace YeildLection;
+public class Programm
+{
+    static void Main()
+    {
+        //Step1-------------------------------------------------------
+        IEnumerator intsEnumerator = GetInts(); // print nothing
+        Console.WriteLine("...");                    // print "..."
+
+        intsEnumerator.MoveNext();                   // print "first"
+        Console.WriteLine(intsEnumerator.Current);   // print 1
+        Console.ReadKey();
+        //------------------------------------------------------------
+
+        PrintFibonacci(5);
+        Console.ReadKey();
+    //return value = generator----------------------------------------
+        IEnumerable enumerable = GetFibonacci(5);
+        IEnumerator enumerator = enumerable.GetEnumerator();
+
+        Console.WriteLine(enumerable == enumerator);
+        Console.ReadKey();
+    }
+
+    #region IEnumerator 
+    static IEnumerator GetInts()
+    {
+        Console.WriteLine("first");
+        yield return 1;
+
+        Console.WriteLine("second");
+        yield return 2;
+    }
+
+    IEnumerator GenerateMultiplicationTable(int maxValue)
+    {
+        for (int i = 2; i <= 10; i++)
+        {
+            for (int j = 2; j <= 10; j++)
+            {
+                int result = i * j;
+
+                if (result > maxValue)
+                    yield break;
+
+                yield return result;
+            }
+        }
+    }
+    #endregion
+
+    #region IEnumerable
+    static void PrintFibonacci(int value)
+    {
+        Console.WriteLine("Fibonacci numbers:");
+
+        foreach (int number in GetFibonacci(value))
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    static IEnumerable GetFibonacci(int maxValue)
+    {
+        int previous = 0;
+        int current = 1;
+
+        while (current <= maxValue)
+        {
+            yield return current;
+
+            int newCurrent = previous + current;
+            previous = current;
+            current = newCurrent;
+        }
+    }
+
+    //-----------------------------------------------
+    int _rangeStart;
+    int _rangeEnd;
+
+    public void TestIEnumerableYield()
+    {
+        IEnumerable polymorphRange = GetRange();
+
+        _rangeStart = 0;
+        _rangeEnd = 3;
+
+        Console.WriteLine(string.Join(' ', polymorphRange)); // 0 1 2 3
+
+        _rangeStart = 5;
+        _rangeEnd = 7;
+
+        Console.WriteLine(string.Join(' ', polymorphRange)); // 5 6 7
+    }
+
+    IEnumerable GetRange()
+    {
+        for (int i = _rangeStart; i <= _rangeEnd; i++)
+        {
+            yield return i;
+        }
+    }
+    #endregion
+}
